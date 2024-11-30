0da36b14259a77056f1f09aa5bf3ea6b58bb03d6
James <james.rosstwo@gmail.com>
2024-11-30 15:41:52 -0500

    add latent space attack

--- sim/perturbation/vae.py
+++ sim/perturbation/vae.py
@@ -1,17 +1,21 @@
 from pathlib import Path
 
 import torch
+from torch import nn
 
 from models.vae import ConvVAE
 from perturbation.perturbation import Perturbation
 
 
 class VAEFramePerturbation(Perturbation):
-    def __init__(self, state_path: Path, z_size: int = 32, perturbation_strenth=0.5):
+    def __init__(self, state_path: Path, z_size: int = 32, perturbation_strength=0.5):
         self._vae = ConvVAE(z_size=z_size)
         vae_state_dict = torch.load(str(state_path))
         self._vae.load_state_dict(vae_state_dict, strict=True)
-        self._vae.encoder.freeze()
+        self._perturbation_strength = perturbation_strength
+
+        for param in self._vae.encoder.parameters():
+            param.requires_grad = False
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         mask = self.get_mask(x)
@@ -22,15 +26,25 @@ class VAEFramePerturbation(Perturbation):
 
 
 class VAELatentPerturation(Perturbation):
-    def __init__(self, state_path: Path, z_size: int = 32, perturbation_strenth=0.5):
+    def __init__(self, state_path: Path, z_size: int = 32, perturbation_strength=0.5):
         self._vae = ConvVAE(z_size=z_size)
         vae_state_dict = torch.load(str(state_path))
         self._vae.load_state_dict(vae_state_dict, strict=True)
-        self._vae.encoder.freeze()
+        self._perturbation_strength = perturbation_strength
+
+        for param in self._vae.encoder.parameters():
+            param.requires_grad = False
+
+        self.latent_mlp = nn.Sequential(
+            nn.Linear(z_size, z_size),
+            nn.ReLU(),
+            nn.Linear(z_size, z_size)
+        )
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         mask = self.get_mask(x)
         latent = self._vae.encoder(x)
+        latent += self.latent_mlp(latent)
         perturbation = self._vae.decoder(latent)
         perturbed = (x * ~mask) + (perturbation * mask)
         return perturbed
